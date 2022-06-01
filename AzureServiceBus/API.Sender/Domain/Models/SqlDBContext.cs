ac893247e61fcb81139b7153c183f7b6e6d84f24
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-06-01 14:45:06 +0200

    Scaffold DbContext from existing Azure Sql Db

---
+++ AzureServiceBus/API.Sender/Domain/Models/SqlDBContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace API.Sender.Domain.Models
+{
+    public partial class SqlDBContext : DbContext
+    {
+        public SqlDBContext()
+        {
+        }
+
+        public SqlDBContext(DbContextOptions<SqlDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:AzureSqlDb");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("USERS");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
