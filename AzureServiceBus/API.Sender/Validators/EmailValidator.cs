4a7b97e0ac90a46d100156c4ec4f61803aef1719
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-06-02 18:55:23 +0200

    Implement email validator

---
+++ AzureServiceBus/API.Sender/Validators/EmailValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Mail;
+
+namespace API.Sender.Validators
+{
+    public static class EmailValidator
+    {
+        public static bool Validate(string email)
+        {
+            try
+            {
+                string emailAddress = new MailAddress(email).Address;
+            }
+
+            catch(FormatException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
